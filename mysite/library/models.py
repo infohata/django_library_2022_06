68d9360d35c845ab621ae95ddee3e730ef136da0
Donatas Noreika <donoras@gmail.com>
2022-06-22 11:47:12 +0300

    Initial commit

---
+++ mysite/library/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+import uuid
+
+
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(verbose_name='Pavadinimas', max_length=200, help_text='Įveskite knygos žanrą (pvz. detektyvas)')
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(verbose_name="Pavadinimas", max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(verbose_name="Aprašymas", max_length=1000, help_text='Trumpas knygos aprašymas')
+    isbn = models.CharField('ISBN', max_length=13,
+                            help_text='13 Simbolių <a href="https://www.isbn-international.org/content/what-isbn">ISBN kodas</a>')
+    genre = models.ManyToManyField('Genre', help_text='Išrinkite žanrą(us) šiai knygai')
+
+    def __str__(self):
+        return self.title
+
+
+class BookInstance(models.Model):
+    unique_id = models.UUIDField(default=uuid.uuid4, help_text='Unikalus ID knygos kopijai')
+    book = models.ForeignKey(to='Book', verbose_name="Knyga", on_delete=models.SET_NULL, null=True)
+    due_back = models.DateField(verbose_name="Bus prieinama", null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('a', 'Administruojama'),
+        ('p', 'Paimta'),
+        ('g', 'Galima paimti'),
+        ('r', 'Rezervuota'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='a', help_text='Statusas')
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
+
+
+class Author(models.Model):
+    first_name = models.CharField('Vardas', max_length=100)
+    last_name = models.CharField('Pavardė', max_length=100)
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
