62c5026674ba0f7abafc7b6853413a5cffb92c1b
Donatas Noreika <donoras@gmail.com>
2022-06-27 09:41:24 +0300

    Added styles

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import (Book,
+                     BookInstance,
+                     Author)
 
-# Create your views here.
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Labas, pasauli!")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='g').count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+    return render(request, 'index.html', context=context)
