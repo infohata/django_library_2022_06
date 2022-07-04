50b86bf1b5f3a2a0362ebd2b712bbfabbbee4705
Kestutis Januskevicius <infohata@gmail.com>
2022-07-04 11:54:35 +0300

    vartotojo registracija done

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,21 +125,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+# and media files
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# media folder settings
-MEDIA_ROOT = os.path.join(BASE_DIR, 'library/media')
-
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
-
 
 TINYMCE_DEFAULT_CONFIG = {
     'height': 360,
@@ -167,3 +168,11 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
     }
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-primary',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
--- mysite/user_profile/views.py
+++ mysite/user_profile/views.py
@@ -3,7 +3,29 @@ from django.contrib.auth import get_user_model
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
 
+
 @csrf_protect
 def register(request):
-
-    return render(request, 'user_profile/register.html')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        is_error = False
+        if not username or get_user_model().objects.filter(username=username).exists():
+            messages.error(request, 'Vartotojo vardas neįvestas, arba toks vartotojas jau egzistuoja.')
+            is_error = True
+        if not email or get_user_model().objects.filter(email=email).exists():
+            messages.error(request, 'El.pašto adresas neįvestas, arba vartotojas su įvestu el.pašto adresu jau egzistuoja.')
+            is_error = True
+        if not password or not password2 or password != password2:
+            messages.error(request, 'Slaptažodžiai nesutampa arba neįvesti.')
+            is_error = True
+        if is_error:
+            return redirect('register')
+        else:
+            get_user_model().objects.create_user(username=username, email=email, password=password)
+            messages.success(request, 'Vartotojas {} sukurtas sėkmingai'.format(username))
+            return redirect('login')
+    else:
+        return render(request, 'user_profile/register.html')
