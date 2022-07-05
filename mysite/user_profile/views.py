84bb669680c37b9b1823ae4e4fd68a77dc9e33bf
Kestutis Januskevicius <infohata@gmail.com>
2022-07-05 10:38:11 +0300

    user profile view

--- mysite/user_profile/views.py
+++ mysite/user_profile/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.password_validation import validate_password, get_password_validators
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
+
+
+@login_required
+def profile(request):
+    return render(request, 'user_profile/profile.html')
 
 
 @csrf_protect
