c78ea86f08e4e89c2479bd59b423f7306eed8fce
Kestutis Januskevicius <infohata@gmail.com>
2022-07-05 13:11:45 +0300

    profile form view done

--- mysite/user_profile/views.py
+++ mysite/user_profile/views.py
@@ -10,8 +10,17 @@ from . forms import UserUpdateForm, ProfileUpdateForm
 
 @login_required
 def profile(request):
-    user_form = UserUpdateForm(instance=request.user)
-    profile_form = ProfileUpdateForm(instance=request.user.profile)
+    if request.method == "POST":
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'User profile was updated successfully.')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
     context = {
         'user_form': user_form,
         'profile_form': profile_form,
