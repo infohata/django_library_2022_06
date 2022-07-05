8917436a6112b2e5a69676f48e80e41180832a62
Kestutis Januskevicius <infohata@gmail.com>
2022-07-05 09:46:37 +0300

    gitignore galore

--- mysite/user_profile/models.py
+++ mysite/user_profile/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        get_user_model(), 
+        on_delete=models.CASCADE,
+        verbose_name='vartotojas',
+        related_name='profile',
+    )
+    picture = models.ImageField(
+        'nuotrauka',
+        default='user/profile/default.png',
+        upload_to='user/profile/pictures',
+    )
+
+    def __str__(self):
+        return '{} profile'.format(str(self.user))
