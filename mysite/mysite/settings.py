d2cc52dbfddc0af4882ad3f8ae04f9815824f654
Kestutis Januskevicius <infohata@gmail.com>
2022-07-07 14:55:42 +0300

    merging PRs with .DS_Store cleaned

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -49,12 +49,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'rosetta',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -116,6 +118,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = [
+    (LANGUAGE_CODE, 'English'),
+    ('lt', 'Lietuvių'),   
+]
 
 TIME_ZONE = 'UTC'
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('tinymce/', include('tinymce.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('rosetta/', include('rosetta.urls')),
     path('', include('library.urls')),
     path('profile/', include('user_profile.urls')),
     # path('', RedirectView.as_view(url='library/', permanent=True)),
